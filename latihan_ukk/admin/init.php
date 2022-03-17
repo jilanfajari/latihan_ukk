29aa0afbb9a4587ffa9bbbc0148490090b641ac6
Muhamad jilannnnn Fajari <93692787+jilanfajari@users.noreply.github.com>
2022-03-17 11:19:15 +0700

    Delete latihan_ukk directory

--- latihan_ukk/admin/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-//menghubungkan ke file App.php
-
-require_once 'core/App.php';
-
-require_once 'core/Controller.php';
--- latihan_ukk/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-//menghubungkan ke file init.php yang ada di folder admin
-require_once '../admin/init.php';
-
-$app = new App;
